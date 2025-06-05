ea6cdb807b20d5fab54e757de4eeccd17968b75d
Media Team <tech@rentamigo.in>
2025-06-05 18:03:01 +0530

    commited

---
+++ src/pages/products/src/ProductApp.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CheckoutPage from './pages/CheckoutPage';
+import Footer from './components/Footer';
+import MiniCart from './components/MiniCart';
+
+const ProductApp = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+      <Footer />
+      <MiniCart />
+    </div>
+  );
+};
+
+export default ProductApp;
