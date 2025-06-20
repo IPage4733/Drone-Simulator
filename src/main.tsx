c4d423e8dafa7281603dc0774a922ff1e530c46a
manasabukkacherla <214g1a0555@srit.ac.in>
2025-06-20 15:20:01 +0530

    googleoauth

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,11 @@ import './pages/Login/src/index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 // 🔁 Replace with your real Google Client ID
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID"
+const GOOGLE_CLIENT_ID = "1057197802246-l9co82fm7hrnih3rb2h52m8gj96mlacj.apps.googleusercontent.com"
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={'917053533064-cd2a4l7jrdn27c4k0k1eoq94tkq3c0hp.apps.googleusercontent.com'}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
